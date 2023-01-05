d4a9046363d90c597415a094ea7480ff5b8d64f3
Anson Ng <914anson5@gmail.com>
2023-01-05 18:06:17 +0800

    feat: add magic number for proved blocks in the chain

--- Blockchain/task/src/Main.kt
+++ Blockchain/task/src/Main.kt
@@ -1,10 +1,38 @@
 package blockchain
 
 import java.security.MessageDigest
+import kotlin.random.Random.Default.nextLong
+import kotlin.math.pow
+import kotlin.system.measureTimeMillis
+
+const val BLOCK_COUNT = 5
 
 class Block(val id: Long = 1, val hashPrev: String = "0") {
+    companion object {
+        private var hashLeadingZeros: Int = 0
+        fun inputLeadingZeros() {
+            print("Enter how many zeros the hash must start with: ")
+            hashLeadingZeros = readln().toInt()
+        }
+    }
+
     val timeCreatedAt: Long = System.currentTimeMillis()
-    val hashBlock: String = applySha256(id.toString() + timeCreatedAt.toString() + hashPrev)
+    var magicNumber: Long = 0
+    var secondsElapsed: Double = 0.0
+    val hashBlock: String = generateHash()
+
+    private fun generateHash(): String {
+        var hash: String
+
+        secondsElapsed = measureTimeMillis {
+            do {
+                magicNumber = nextLong(1, 10f.pow(hashLeadingZeros + 3).toLong())
+                hash = applySha256(id.toString() + timeCreatedAt.toString() + hashPrev + magicNumber.toString())
+            } while (hash.slice(0 until hashLeadingZeros) != "0".repeat(hashLeadingZeros))
+        } / 1000.0
+
+        return hash
+    }
 }
 
 class Blockchain {
@@ -18,17 +46,30 @@ class Blockchain {
 
     fun printChain() {
         for (block in chain) {
-            println("Block:")
-            println("Id: ${block.id}")
-            println("Timestamp: ${block.timeCreatedAt}")
-            println("Hash of the previous block:\n${block.hashPrev}")
-            println("Hash of the block:\n${block.hashBlock}\n")
+            println(
+                """
+            Block:
+            Id: ${block.id}
+            Timestamp: ${block.timeCreatedAt}
+            Magic number: ${block.magicNumber}
+            Hash of the previous block:
+            ${block.hashPrev}
+            Hash of the block:
+            ${block.hashBlock}
+            Block was generating for ${block.secondsElapsed} seconds
+            
+            """.trimIndent()
+            )
         }
     }
 }
 
 fun main() {
+    Block.inputLeadingZeros()
+
     val chain = Blockchain()
+
+    chain.appendChain(BLOCK_COUNT - 1)
 
     chain.printChain()
 }
